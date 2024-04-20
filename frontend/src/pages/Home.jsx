09a3ed8b9cf6c35181fbcb07c1371da6c11ab322
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-04-20 21:18:55 +0530

    added pages and react router

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      HOmee
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign IN</h1>
+    </div>
+  )
+}
+
+export default SignIn
