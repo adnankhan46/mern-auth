19b8adaad8d12203b3bfe3b5263a2e894685e72a
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-04-20 21:42:28 +0530

    Created Header Component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div className='bg-slate-200'>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <Link to="/">
+      <h1 className='font-bold hover:text-blue-500'>Auth App</h1>
+      </Link>
+      <ul className='flex gap-4'>
+      <Link to="/">
+      <li>Home</li>
+      </Link>
+      <Link to="/about">
+      <li>About</li>
+      </Link>
+      <Link to="/signin">
+      <li>SignIN</li>
+      </Link>
+      </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
