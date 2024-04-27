5b0c20754b453b74dd9f91c5aca65e8249409978
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-04-27 20:56:44 +0530

    Added SignUp and used bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 // Importing Routes
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+// Middleware modules
+app.use(express.json());
 
 // User Routes
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MONGODB")
