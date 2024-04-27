78f051d92ed67b948b120272ecb27c74388d92b0
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-04-27 19:34:24 +0530

    Created Test API wuth MVC Architecture

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+// Importing Routes
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
+const app = express();
+
+// User Routes
+app.use("/api/user", userRoutes);
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MONGODB")
@@ -10,8 +16,14 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 
-const app = express();
+
+app.get("/helloworld", (req, res)=>{
+    res.json({
+        message: "Hello World"
+    });
+})
+
 
 app.listen(3000, ()=>{
     console.log("Server listening on PORT 3000");
-})
+});
